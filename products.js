0ec4e9cc3c164cd0a1c865bb2d8ad53073039123
candientuthongminh <qtnglobal.com@gmail.com>
2026-05-06 16:28:58 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,5 +1,5 @@
 const products = [
-    {id:1,name:"Cân bàn 300kg",price:2500000,image:"images/can1.jpg",desc:"Cân bàn công nghiệp"},
+    {id:1,name:"Cân điện tử VIBRA ALC ",price:2500000,image:"images/can-dem/vibra-alc/1.jpg",desc:"Cân bàn"},
     {id:2,name:"Cân sàn 1 tấn",price:4500000,image:"images/can2.jpg",desc:"Cân sàn tải lớn"},
     {id:3,name:"Cân chống nước IP68",price:5200000,image:"images/can3.jpg",desc:"Cân thủy sản"}
 ];
