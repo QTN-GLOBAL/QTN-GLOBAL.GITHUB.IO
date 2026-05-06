88b36249676e96d3f80ed34b74128e0c033c1a4a
candientuthongminh <qtnglobal.com@gmail.com>
2026-05-06 15:04:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-const list = document.getElementById("product-list");
+const list = document.getElementById("list");
 
-/* render */
-function renderProducts(data){
-  list.innerHTML = "";
+function render(data){
+  list.innerHTML="";
   data.forEach(p=>{
-    list.innerHTML += `
-      <div class="card">
-        <img src="${p.images[0]}" onerror="this.src='images/no-image.png'">
-        <h3>${p.name}</h3>
-        <a href="chitiet.html?id=${p.id}" class="btn">Chi tiết</a>
-      </div>
-    `;
+    list.innerHTML+=`
+    <div class="card">
+      <img src="${p.images[0]}" onerror="this.src='images/no-image.png'">
+      <h3>${p.name}</h3>
+      <a href="chitiet.html?id=${p.id}">Chi tiết</a>
+    </div>`;
   });
 }
 
-/* hero slider */
-let i = 0;
-const heroImg = document.getElementById("hero-img");
+function filter(cat){
+  if(cat==="all") render(products);
+  else render(products.filter(p=>p.category===cat));
+}
 
-function runHero(){
+/* slider */
+const hero=document.getElementById("hero-img");
+let i=0;
+
+if(hero){
+  hero.src=products[0].images[0];
   setInterval(()=>{
-    i++;
-    if(i >= products.length) i = 0;
-
-    heroImg.src = products[i].images[0];
+    i=(i+1)%products.length;
+    hero.src=products[i].images[0];
   },2500);
 }
 
-heroImg.src = products[0].images[0];
-runHero();
-
-renderProducts(products);
+render(products);
