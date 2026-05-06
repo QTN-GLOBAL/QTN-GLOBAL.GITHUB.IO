fd81cf4b33058df7671f3c2f944f16c447050361
candientuthongminh <qtnglobal.com@gmail.com>
2026-05-06 12:52:16 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,45 +1,22 @@
 const products = [
   {
     id: 1,
-    name: "Cân điện tử JCL",
+    name: "Cân bàn JWL",
     category: "can-ban",
 
     images: [
-      "images/jcl/main.jpg",
-      "images/jcl/1.jpg",
-      "images/jcl/2.jpg"
+      "images/can-ban/jwl/main.jpg",
+      "images/can-ban/jwl/1.jpg"
     ],
 
     brand: "Jadever",
     origin: "Đài Loan",
 
-    desc: "Cân bàn điện tử độ chính xác cao.",
+    desc: "Cân bàn điện tử phổ thông.",
 
     specs: [
-      "Tải trọng: 30kg",
-      "Sai số: 1g",
-      "Màn hình LED"
-    ]
-  },
-
-  {
-    id: 2,
-    name: "Cân Vibra ALC",
-    category: "can-dem",
-
-    images: [
-      "images/vibra-alc/main.jpg",
-       "images/vibra-alc/1.jpg"
-    ],
-
-    brand: "Vibra",
-    origin: "Nhật Bản",
-
-    desc: "Cân đếm linh kiện chính xác.",
-
-    specs: [
-      "Đếm nhanh",
-      "Độ chính xác cao"
+      "Tải trọng: 60kg",
+      "Sai số: 5g"
     ]
   }
 ];
