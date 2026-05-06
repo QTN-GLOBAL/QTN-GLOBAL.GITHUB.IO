34e7334ae4f7b497d83ff7bc38040f06210ea79a
candientuthongminh <qtnglobal.com@gmail.com>
2026-05-06 12:53:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const list = document.getElementById("product-list");
 
-/* RENDER */
+/* render */
 function renderProducts(data){
   list.innerHTML = "";
   data.forEach(p=>{
     list.innerHTML += `
       <div class="card">
-        <img src="${p.images[0]}">
+        <img src="${p.images[0]}" onerror="this.src='images/no-image.png'">
         <h3>${p.name}</h3>
         <a href="chitiet.html?id=${p.id}" class="btn">Chi tiết</a>
       </div>
@@ -14,13 +14,7 @@ function renderProducts(data){
   });
 }
 
-/* FILTER */
-function filterProduct(c){
-  if(c==="all") renderProducts(products);
-  else renderProducts(products.filter(p=>p.category===c));
-}
-
-/* HERO SLIDER */
+/* hero slider */
 let i = 0;
 const heroImg = document.getElementById("hero-img");
 
@@ -28,6 +22,7 @@ function runHero(){
   setInterval(()=>{
     i++;
     if(i >= products.length) i = 0;
+
     heroImg.src = products[i].images[0];
   },2500);
 }
