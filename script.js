8720f445a5fea264e04c8106894fdfa5c632e19e
candientuthongminh <qtnglobal.com@gmail.com>
2026-05-12 11:37:32 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -124,3 +124,63 @@ function closeCartPopup(){
     .style.display = "none";
 
 }
+function renderWeightLevels(){
+
+    const orderLevels =
+    document.getElementById("orderLevels");
+
+    const cartLevels =
+    document.getElementById("cartLevels");
+
+    if(!orderLevels || !cartLevels) return;
+
+    orderLevels.innerHTML = "";
+    cartLevels.innerHTML = "";
+
+    let levels = [];
+
+    product.specs.forEach(spec=>{
+
+        if(spec.includes("kg") || spec.includes("tấn")){
+
+            const splitLevels =
+            spec.split(":");
+
+            if(splitLevels.length > 1){
+
+                levels =
+                splitLevels[1]
+                .split("/")
+                .map(x=>x.trim());
+
+            }
+
+        }
+
+    });
+
+    levels.forEach(level=>{
+
+        orderLevels.innerHTML += `
+        <div class="cart-row">
+            <span>${level}</span>
+            <input type="number"
+                   min="0"
+                   value="0">
+        </div>
+        `;
+
+        cartLevels.innerHTML += `
+        <div class="cart-row">
+            <span>${level}</span>
+            <input type="number"
+                   min="0"
+                   value="0">
+        </div>
+        `;
+
+    });
+
+}
+
+renderWeightLevels();
