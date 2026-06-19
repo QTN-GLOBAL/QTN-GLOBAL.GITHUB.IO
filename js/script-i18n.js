4f67fa5ead5eccf241a7b3537d37dd3bb7d60449
QTN-GLOBAL <qtnglobal.com@gmail.com>
2026-06-19 10:37:51 +0700

    Add files via upload

--- js/script-i18n.js
+++ js/script-i18n.js
@@ -37,7 +37,7 @@ copiedOrderOpenMessenger:
         heroFeature2: "BÁO GIÁ NHANH - GIAO HÀNG TOÀN QUỐC",
         heroFeature3: "HỖ TRỢ KỸ THUẬT CHUYÊN NGHIỆP",
 
-        categories: "DANH MỤC",
+        product categories: "DANH MỤC SẢN PHẨM",
 
         catPlatformScale: "Cân bàn",
         catCountingScale: "Cân đếm",
@@ -127,7 +127,7 @@ copiedOrderOpenMessenger:
         heroFeature2: "FAST QUOTATION - NATIONWIDE DELIVERY",
         heroFeature3: "PROFESSIONAL TECHNICAL SUPPORT",
 
-        categories: "CATEGORIES",
+        product categories: "PRODUCT CATEGORIES",
 
         catPlatformScale: "Platform Scales",
         catCountingScale: "Counting Scales",
@@ -218,7 +218,7 @@ copiedOrderOpenMessenger:
         heroFeature2: "快速报价 - 全国配送",
         heroFeature3: "专业技术支持",
 
-        categories: "产品分类",
+        product categories: "产品分类",
 
         catPlatformScale: "台秤",
         catCountingScale: "计数秤",
