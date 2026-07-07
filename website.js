426494816f97d4b1ca9f4e3254f506c4ae0f41b0
QTN-GLOBAL <qtnglobal.com@gmail.com>
2026-07-07 10:46:19 +0700

    Add files via upload

---
+++ website.js
@@ -0,0 +1,19 @@
+function renderWebsite(){
+
+    const content = document.getElementById("dashboardContent");
+
+    if(!content) return;
+
+    content.innerHTML = `
+
+        <h1>🌐 WEBSITE</h1>
+
+        <p>
+
+            Đây là trung tâm quản lý Website QTN GLOBAL.
+
+        </p>
+
+    `;
+
+}
