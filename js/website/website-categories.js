412b57fb800d63bc30c713cde34cfa61c2a38fbe
QTN-GLOBAL <qtnglobal.com@gmail.com>
2026-07-09 13:56:26 +0700

    Delete js/website/website-categories.js

--- js/website/website-categories.js
+++
@@ -1,8 +0,0 @@
-function renderWebsiteCategories(){
-
-    document.getElementById("websiteWorkspace").innerHTML = `
-        <h2>📂 Categories</h2>
-        <p>Đây sẽ là nơi quản lý danh mục.</p>
-    `;
-
-}
