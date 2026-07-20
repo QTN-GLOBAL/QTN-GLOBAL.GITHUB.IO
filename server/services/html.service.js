1cdfe9a36ed551a005bb91d75c5af3ff200bb6e4
QTN-GLOBAL <qtnglobal.com@gmail.com>
2026-07-20 13:32:41 +0700

    Update html.service.js

--- server/services/html.service.js
+++ server/services/html.service.js
@@ -3,9 +3,14 @@
 ========================================== */
 
 import axios from "axios";
+import * as cheerio from "cheerio";
 
 const USER_AGENT =
 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/138.0 Safari/537.36";
+
+/* ==========================================
+   DOWNLOAD HTML
+========================================== */
 
 export async function downloadHtml(url) {
 
@@ -18,6 +23,12 @@ export async function downloadHtml(url) {
     console.log("===================================");
 
     try {
+
+        if (!url.startsWith("http")) {
+
+            throw new Error("Invalid URL");
+
+        }
 
         const response = await axios.get(url, {
 
@@ -34,13 +45,25 @@ export async function downloadHtml(url) {
 
         });
 
+        const html = response.data;
+
+        const $ = cheerio.load(html);
+
+        const title = $("title").first().text().trim();
+
+        console.log("PAGE TITLE:");
+
+        console.log(title);
+
         return {
 
             success: true,
 
             url,
 
-            html: response.data
+            title,
+
+            html
 
         };
 
@@ -48,13 +71,15 @@ export async function downloadHtml(url) {
 
     catch (error) {
 
-        console.error(error.message);
+        console.error(error);
 
         return {
 
             success: false,
 
             url,
+
+            title: "",
 
             html: "",
 
