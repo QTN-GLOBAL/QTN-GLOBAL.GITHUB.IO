592709d0ee476db55b2bc78d53c58961e94ea6c5
QTN-GLOBAL <qtnglobal.com@gmail.com>
2026-07-20 12:58:43 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 /* ==========================================
    QTN GLOBAL AI IMPORT SERVER
 ========================================== */
-
+import importRoute from "./routes/import.route.js";
 import express from "express";
 import cors from "cors";
 
@@ -22,6 +22,7 @@ app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/import", importRoute);
 
 /* ==========================================
    HOME
@@ -41,31 +42,7 @@ app.get("/", (req, res) => {
 
 });
 
-/* ==========================================
-   TEST IMPORT
-========================================== */
 
-app.post("/api/import", async (req, res) => {
-
-    console.log("");
-
-    console.log("========== IMPORT REQUEST ==========");
-
-    console.log(req.body);
-
-    console.log("====================================");
-
-    res.json({
-
-        success: true,
-
-        message: "Import API Ready",
-
-        data: req.body
-
-    });
-
-});
 
 /* ==========================================
    START
